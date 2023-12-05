9c53dd6ef9491a22fa903272bee81cdf7a9ec238
gyorm <gyorm@ajou.ac.kr>
2023-12-05 15:45:29 +0900

    231205 15:45 commit

--- Santa_Game/label/rudolphPointLabel.gd
+++ Santa_Game/label/rudolphPointLabel.gd
@@ -4,24 +4,29 @@ var rudolphPoints = 0
 
 func _ready():
 	text = "Rudolph Points: "+str(rudolphPoints)+"/50"
+#
+#func _on_prison_rudolph_rescued():
+#	rudolphPoints = rudolphPoints + 10
+#	_ready()
+#
+#func _on_prison_2_rudolph_rescued():
+#	rudolphPoints = rudolphPoints + 10
+#	_ready()
+#
+#func _on_prison_3_rudolph_rescued():
+#	rudolphPoints = rudolphPoints + 10
+#	_ready()
+#
+#func _on_prison_4_rudolph_rescued():
+#	rudolphPoints = rudolphPoints + 10
+#	_ready()
+#
+#func _on_prison_5_rudolph_rescued():
+#	rudolphPoints = rudolphPoints + 10
+#	_ready()
 
-func _on_prison_rudolph_rescued():
+
+
+func _on_player_1_rudolph_rescued():
 	rudolphPoints = rudolphPoints + 10
 	_ready()
-
-func _on_prison_2_rudolph_rescued():
-	rudolphPoints = rudolphPoints + 10
-	_ready()
-
-func _on_prison_3_rudolph_rescued():
-	rudolphPoints = rudolphPoints + 10
-	_ready()
-
-func _on_prison_4_rudolph_rescued():
-	rudolphPoints = rudolphPoints + 10
-	_ready()
-
-func _on_prison_5_rudolph_rescued():
-	rudolphPoints = rudolphPoints + 10
-	_ready()
-
