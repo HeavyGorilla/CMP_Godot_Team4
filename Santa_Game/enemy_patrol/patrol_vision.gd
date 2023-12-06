0249d50c7e4127e05c87be040fb12246c0032877
HeavyGorilla <127929175+HeavyGorilla@users.noreply.github.com>
2023-12-07 04:48:33 +0900

    Add files via upload

--- Santa_Game/enemy_patrol/patrol_vision.gd
+++ Santa_Game/enemy_patrol/patrol_vision.gd
@@ -9,11 +9,14 @@ func _ready():
 # 플레이어가 시야 범위 내에 들어왔을 때 호출
 func _on_body_entered(body):
 	if body.is_in_group("santa"):
-		# RayCast2D의 타겟 위치를 플레이어 위치로 설정
-		var direction_to_player = (body.global_position - global_position).normalized()
-		ray_cast.target_position = direction_to_player * vision_distance
-		ray_cast.force_raycast_update()
-		if ray_cast.is_colliding() and ray_cast.get_collider() == body:
-			print("플레이어 감지됨!")
-			
-			# 플레이어 == patrol visoin Mask
+		update_ray_cast_direction(body)
+
+func update_ray_cast_direction(target_body):
+	# 경비병의 현재 방향에 따라 RayCast2D의 cast_to 설정
+	var direction = (target_body.global_position - global_position).normalized()
+	ray_cast.target_position = direction * vision_distance
+	ray_cast.force_raycast_update()
+
+	# RayCast2D 감지 확인
+	if ray_cast.is_colliding() and ray_cast.get_collider() == target_body:
+		print("플레이어 감지됨!")
