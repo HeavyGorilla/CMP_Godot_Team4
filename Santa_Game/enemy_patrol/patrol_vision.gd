5356a692e96c684399c055a4832850a8e6da05fc
HeavyGorilla <127929175+HeavyGorilla@users.noreply.github.com>
2023-12-08 10:52:54 +0900

    Add files via upload

--- Santa_Game/enemy_patrol/patrol_vision.gd
+++ Santa_Game/enemy_patrol/patrol_vision.gd
@@ -20,3 +20,4 @@ func update_ray_cast_direction(target_body):
 	# RayCast2D 감지 확인
 	if ray_cast.is_colliding() and ray_cast.get_collider() == target_body:
 		print("플레이어 감지됨!")
+		get_tree().change_scene_to_file("res://game/gameover.tscn")
