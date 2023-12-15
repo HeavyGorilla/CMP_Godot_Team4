a8561e8f3f424508cf1c19ed8ccab5db43ced06f
g0r0kke <141205355+g0r0kke@users.noreply.github.com>
2023-12-15 17:18:40 +0900

    Add files via upload

--- Santa_Game/player/Player1.gd
+++ Santa_Game/player/Player1.gd
@@ -111,7 +111,6 @@ func _on_area_2d_body_exited(body):
 
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("prison"):
-		print("감옥 터치")
 		if touched_rudolph == null:
 			touched_rudolph = area
 
@@ -122,7 +121,17 @@ func _on_area_2d_area_exited(area):
 			touched_rudolph = null
 			
 func _input(event):
+
 	if touched_rudolph != null and event.is_action_pressed("rescue"):
 		emit_signal("rudolph_rescued")
-		print("루돌프 구출")
+		if touched_rudolph == $"../Prison1":
+			print("감옥1 구출")
+		if touched_rudolph == $"../Prison2":
+			print("감옥2 구출")
+		if touched_rudolph == $"../Prison3":
+			print("감옥3 구출")
+		if touched_rudolph == $"../Prison4":
+			print("감옥4 구출")
+		if touched_rudolph == $"../Prison5":
+			print("감옥5 구출")
 		touched_rudolph.queue_free()
