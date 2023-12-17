549a36e2247372f66e37596db2d564730b7addd0
saybeok <58909145+saybeok@users.noreply.github.com>
2023-12-17 16:45:02 +0900

    Update main.gd

--- Santa_Game/firstpage/main.gd
+++ Santa_Game/firstpage/main.gd
@@ -3,7 +3,8 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if Firstmusic.playing == false:
+		Firstmusic.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
