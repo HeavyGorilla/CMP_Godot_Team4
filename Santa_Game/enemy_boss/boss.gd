61bfee76bf1c960b15caa8fb1eec73cd2043a0db
saybeok <58909145+saybeok@users.noreply.github.com>
2023-12-17 16:35:43 +0900

    Add files via upload

--- Santa_Game/enemy_boss/boss.gd
+++ Santa_Game/enemy_boss/boss.gd
@@ -119,6 +119,7 @@ func get_player_position() -> Vector2:
 func take_damage(damage_amount):
 	HP -= damage_amount
 	animation.play("boss_hurt")
+	$AudioStreamPlayer2D.play()
 	if HP <= 0:
 		die()
 		get_tree().change_scene_to_file("res://Game/stage2clear.tscn")
