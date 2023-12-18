49967be354f71814f954721e283d01665e1c08e4
g0r0kke <141205355+g0r0kke@users.noreply.github.com>
2023-12-18 19:38:42 +0900

    효과음 추가

--- Santa_Game/label/PointLabel.gd
+++ Santa_Game/label/PointLabel.gd
@@ -6,17 +6,21 @@ func _ready():
 	text = "Points: "+str(Points)
 
 func _on_player_1_rudolph_rescued():
+	$"/root/Rescue".play()
 	Points = Points + 50
 	_ready()
 
 func _on_gift_a_point_label_gift_apoint_up():
+	$"/root/Hohoho".play()
 	Points = Points + 100
 	_ready()
 
 func _on_gift_b_point_label_gift_bpoint_up():
+	$"/root/Hohoho".play()
 	Points = Points + 100
 	_ready()
 
 func _on_gift_c_point_label_gift_cpoint_up():
+	$"/root/Hohoho".play()
 	Points = Points + 100
 	_ready()
