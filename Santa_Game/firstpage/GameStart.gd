a191d901038f10848af1eadc0db8905fc2c1eb5d
saybeok <58909145+saybeok@users.noreply.github.com>
2023-12-18 22:45:32 +0900

    Add files via upload

--- Santa_Game/firstpage/GameStart.gd
+++ Santa_Game/firstpage/GameStart.gd
@@ -10,3 +10,4 @@ func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		get_tree().change_scene_to_file("res://level/Level1.tscn")
 		Firstmusic.stop()
+		Stage1Music.play_sound()
