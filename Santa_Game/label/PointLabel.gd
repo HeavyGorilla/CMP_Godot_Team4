1a64ea7a030ea3518de42e8727d67322f777d157
g0r0kke <141205355+g0r0kke@users.noreply.github.com>
2023-12-18 18:44:36 +0900

    선물=100점, 루돌프=50점

--- Santa_Game/label/PointLabel.gd
+++ Santa_Game/label/PointLabel.gd
@@ -6,17 +6,17 @@ func _ready():
 	text = "Points: "+str(Points)
 
 func _on_player_1_rudolph_rescued():
-	Points = Points + 10
+	Points = Points + 50
 	_ready()
 
 func _on_gift_a_point_label_gift_apoint_up():
-	Points = Points + 10
+	Points = Points + 100
 	_ready()
 
 func _on_gift_b_point_label_gift_bpoint_up():
-	Points = Points + 10
+	Points = Points + 100
 	_ready()
 
 func _on_gift_c_point_label_gift_cpoint_up():
-	Points = Points + 10
+	Points = Points + 100
 	_ready()
